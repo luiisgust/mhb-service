fc8e7fd73b5714a2e8bedffcb8bb416fc22e0550
Luis Gustavo <gustavofon789@gmail.com>
2026-05-08 15:45:36 -0300

    att

--- src/controllers/prodecimentosController.js
+++ src/controllers/prodecimentosController.js
@@ -21,21 +21,4 @@ module.exports = (app) => {
             res.status(500).json({ success: false, msg: error.message });
         }
     });
-
-    app.delete("/procedimento/:id", async (req, res) => {
-        try {
-            const rowCount = await ProcedimentoDAO.excluir(req.params.id);
-            if (rowCount > 0) {
-                res.json({ success: true, msg: "Procedimento removido com sucesso!" });
-            } else {
-                res.status(404).json({ success: false, msg: "Procedimento não encontrado." });
-            }
-        } catch (error) {
-            res.status(400).json({ 
-                success: false, 
-                msg: "Erro ao excluir: Verifique se este procedimento possui agendamentos ou vendas vinculadas.",
-                details: error.message
-            });
-        }
-    });
 }
